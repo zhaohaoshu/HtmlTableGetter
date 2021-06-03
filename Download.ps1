ec0a8d604bcff81298deb72de9b4e1d574a95fa5
zhaohaoshu <zhaohaoshu@gmail.com>
2021-06-03 21:08:45 +0000

    Update Download.ps1

--- Download.ps1
+++ Download.ps1
@@ -1,3 +1,3 @@
-﻿Invoke-WebRequest -Uri https://github.com/zhaohaoshu/HtmlTableGetter/archive/refs/heads/main.zip -OutFile .\HtmlTableGetter.zip
+Invoke-WebRequest -Uri https://github.com/zhaohaoshu/HtmlTableGetter/archive/refs/heads/main.zip -OutFile .\HtmlTableGetter.zip
 Expand-Archive -Path .\HtmlTableGetter.zip -Force
 Compress-Archive -Path .\HtmlTableGetter\HtmlTableGetter-main\src\* -DestinationPath .\HtmlTableGetter\HtmlTableGetter.zip -Force
