038a049cb7cde05bf931cbf2384f6eaf1f91b9e2
zhaohaoshu <zhaohaoshu@gmail.com>
2021-06-05 05:23:02 +0000

    Update Download.ps1

--- Download.ps1
+++ Download.ps1
@@ -1,3 +1,17 @@
-Invoke-WebRequest -Uri https://github.com/zhaohaoshu/HtmlTableGetter/archive/refs/heads/main.zip -OutFile .\HtmlTableGetter.zip
-Expand-Archive -Path .\HtmlTableGetter.zip -Force
-Compress-Archive -Path .\HtmlTableGetter\HtmlTableGetter-main\src\* -DestinationPath .\HtmlTableGetter\HtmlTableGetter.zip -Force
+param(
+    $path = (Get-Location)
+)    
+
+[System.AppContext]::SetSwitch('Switch.System.IO.Compression.ZipFile.UseBackslash', $false)
+
+$downloadedZipFileName = Join-Path $path HtmlTableGetter.zip
+Invoke-WebRequest -Uri https://github.com/zhaohaoshu/HtmlTableGetter/archive/refs/heads/main.zip -OutFile $downloadedZipFileName
+
+$expandedDirectoryName = Join-Path $path HtmlTableGetter
+Remove-Item -Recurse $expandedDirectoryName -ErrorAction Ignore
+Expand-Archive -Path $downloadedZipFileName -DestinationPath $expandedDirectoryName
+
+$sourceDirectoryName = Join-Path $expandedDirectoryName HtmlTableGetter-main\src
+$destinationArchiveFileName = Join-Path $path HtmlTableGetter\HtmlTableGetter.zip
+Remove-Item $destinationArchiveFileName -ErrorAction Ignore
+[System.IO.Compression.ZipFile]::CreateFromDirectory($sourceDirectoryName, $destinationArchiveFileName)
